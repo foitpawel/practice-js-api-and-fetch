d8cf69193e06dee147e65e4732042da1798bb9aa
fawelp <foitpawel@gmail.com>
2024-07-26 16:52:54 +0200

    Use Promise and add callback

--- 02/app.js
+++ 02/app.js
@@ -1,32 +1,39 @@
 document.addEventListener('DOMContentLoaded', init);
 
-function init() {
-    const divList = document.querySelectorAll('div');
-    
-    setBorderColorAsync(divList[0], 'red', function() {
-        setBorderColorAsync(divList[1], 'blue', function() {
-            setBorderColorAsync(divList[2], 'green', function() {
-                console.log('finish');
-            });
-        });
-    });
+async function init() {
+  const divList = document.querySelectorAll('div');
+  try {
+    await setBordersSequentially(divList, ['red', 'blue', 'green'], setBorderColor);
+    console.log('finish');
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+}
 
+async function setBordersSequentially(elements, colors, callback) {
+  for (let i = 0; i < elements.length; i++) {
+    await setBorderColorAsync(elements[i], colors[i], callback);
+  }
+}
+
+function setBorderColor(element, color) {
+  element.style.border = `3px solid ${color}`;
 }
 
 function setBorderColorAsync(element, color, callback) {
-    if(element && element instanceof HTMLElement) {
-        // sprawdzam czy parametr jest elementem DOM, więcej:
-        // https://stackoverflow.com/questions/384286/javascript-isdom-how-do-you-check-if-a-javascript-object-is-a-dom-object
-        
-        if(callback && typeof callback === 'function') {
-            setTimeout(() => {
-                element.style.border = `3px solid ${color}`;
-                callback();
-            }, Math.random() * 3000);
-        } else {
-            alert('Parametr ~callback~ mus być funkcją');
+  return new Promise((resolve, reject) => {
+    if (element && element instanceof HTMLElement) {
+      setTimeout(() => {
+        if (callback && typeof callback === 'function') {
+          resolve(callback(element, color));
         }
+      }, Math.random() * 3000);
     } else {
-        alert('Paremetr ~element~ musi być prawidłowym elementem DOM');
+      const error = new Error('Parametr ~element~ musi być prawidłowym elementem DOM');
+      if (callback && typeof callback === 'function') {
+        callback(error);
+      }
+      reject(error);
     }
+  });
 }
