d0d177edc43c25dbfaf6591412f115ea85d24230
fawelp <foitpawel@gmail.com>
2024-07-30 13:09:53 +0200

    Get Api and change span text

--- 03/app.js
+++ 03/app.js
@@ -1,5 +1,30 @@
 document.addEventListener('DOMContentLoaded', init);
 
 function init() {
-    console.log('DOM');
+  const button = document.getElementsByTagName('button')[0];
+  button.addEventListener('click', handleChangeText)
+
+}
+
+const handleChangeText = async () => {
+  const span = document.getElementsByTagName('span');
+  try {
+    const api = await getApi();
+    span[0].innerHTML = api;
+  } catch (error) {
+    console.log('Error:', error);
+    span[0].innerHTML = 'Error fetching IP';
+  }
+}
+
+const getApi = async () => {
+  return fetch('https://api.ipify.org?format=json')
+    .then(response => response.json())
+    .then(data => {
+        return data.ip;
+    })
+    .catch(error => {
+      console.log('Error:', error);
+      throw error;
+    });
 }
