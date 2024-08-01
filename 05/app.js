39262af921a8c62bde13fd15a4b31bab78316998
fawelp <foitpawel@gmail.com>
2024-08-01 17:01:59 +0200

    Add Post function and get-form-data

--- 05/app.js
+++ 05/app.js
@@ -4,34 +4,76 @@ document.addEventListener('DOMContentLoaded', init);
 
 function init() {
     loadUsers();
+    const element = document.getElementsByClassName('form')[0];
+
+    element.addEventListener("submit", async evt => {
+      const data = await getFormData(evt);
+      await fetchPost(data);
+    });
 }
+
+const getFormData = evt => {
+  evt.preventDefault();
+  const formData = new FormData(evt.target);
+  const data = Array.from(formData.entries()).reduce((acc, [key, value]) => {
+    acc[key] = value;
+    return acc;
+  }, {});
+
+  return data
+}
+
+async function fetchPost(body) {
+  try {
+    const response = await fetch(apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        firstName: body.firstName,
+        lastName: body.lastName,
+      })
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log('Fetch error:', err);
+  }
+}
+
 
 function loadUsers() {
-    const promise = fetchGet(apiUrl);
+  const promise = fetchGet(apiUrl);
 
-    promise
-        .then(data => insertUsers(data))
-        .catch(err => console.error(err));
+  promise
+      .then(data => insertUsers(data))
+      .catch(err => console.error(err));
 }
 
-function fetchGet(url) {
-    return fetch(url)
-        .then(resp => {
-            if(resp.ok) {
-                return resp.json();
-            }
+async function fetchGet(url) {
+  return fetch(url)
+    .then(resp => {
+      if(resp.ok) {
+        return resp.json();
+      }
 
-            return Promise.reject(resp);
-        });
+      return Promise.reject(resp);
+  });
 }
 
 function insertUsers(usersList) {
-    const ulElement = document.querySelector('.users');
-    ulElement.innerHTML = '';
-    usersList.forEach(user => {
-        const liElement = document.createElement('li');
-        liElement.innerText = `${user.firstName} ${user.lastName}`;
+  const ulElement = document.querySelector('.users');
+  ulElement.innerHTML = '';
+  usersList.forEach(user => {
+    const liElement = document.createElement('li');
+    liElement.innerText = `${user.firstName} ${user.lastName}`;
 
-        ulElement.appendChild(liElement);
-    });
+    ulElement.appendChild(liElement);
+  });
 }
